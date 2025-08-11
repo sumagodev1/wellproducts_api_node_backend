04b986f28b69964829db4ce75908b950b81302c2
Shankar Tile <shankartile@gmail.com>
2025-08-11 11:18:09 +0530

    Change in News page

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -8,7 +8,7 @@ const upload = multer({ dest: "uploads/news/" }); // or custom storage
 router.post("/create-news", upload.single("img"), newsController.addNewsEvent);
 router.put("/update-news/:id", upload.single("img"), newsController.updateNewsEvent);
 router.get("/get-news", newsController.getNewsEvents);
-router.patch("/toggle-active/:id", newsController.toggleActiveStatus);
+router.put("/toggle-active/:id", newsController.toggleActiveStatus);
 router.delete("/isdelete-news/:id", newsController.toggleDeleteStatus);
 
 module.exports = router;
